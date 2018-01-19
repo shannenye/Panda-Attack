e18efe0e929ad2d24a3d0639f88e8fd7354a6d97
Shannen <shannen.ye@gmail.com>
2018-01-18 19:22:46 -0500

    Created boilerplate and functioning html site

---
+++ README.md
@@ -0,0 +1,6 @@
+Attack of the Pandas!
+
+Your Panda caretaker is stuck in the middle of the zoo and he's just run out of bamboo!
+Use the floating arrow to shoo the hungry pandas away before they reach the caretaker. If
+your caretaker gets bit 5 times before help arrives then you lose! You only need to hold
+off the fluffy munchers for 2 minutes, good luck!
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const volleyball = require('volleyball');
+const path = require('path');
+
+const app = express();
+
+app.use(volleyball);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res, next) => {
+    res.send('./public/index.html')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error, something went wrong in your file');
+});
+
+const PORT = 9999;
+app.listen(process.env.PORT || 9999, () => {
+    console.log(`Your app.listen is working, connection established on ${PORT}`);
+});
