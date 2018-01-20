80b4ddcd85e73c041f98e3e06f0f08ceae3c1752
Shannen <shannen.ye@gmail.com>
2018-01-20 18:47:31 -0500

    created countdown timer and life on screen

--- public/Game/main.js
+++ public/Game/main.js
@@ -2,6 +2,11 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'phaser-example', { preload: 
 var swatter;
 var person;
 var pandas;
+var gameOver;
+var score = 100;
+var scoreText;
+var timer;
+var total = 2000;
 
 function preload() {
     game.load.image('background', '../images/bamboo.jpg');
@@ -9,6 +14,7 @@ function preload() {
     game.load.image('heart', '../images/heart.png');
     game.load.image('swatter', '../images/pandaSwatter.png');
     game.load.spritesheet('pandas', '../images/pandas.png');
+    game.load.image('gameOver', '../images/Game_Over.png');
 
 }
 
@@ -33,6 +39,9 @@ function create() {
     swatter.height = 80;
     swatter.anchor.setTo(0, 0);
 
+    timer = game.time.create(false);
+    timer.start();
+
     pandas = game.add.emitter(0, 0, 250);
     pandas.makeParticles('pandas', 10, 200, true, true);
     pandas.maxParticleSpeed.setTo(200, -400);
@@ -43,34 +52,36 @@ function create() {
 
     game.physics.enable(swatter);
 
-
     swatter.body.allowRotation = false;
+
+    scoreText = game.add.text(700, 10, `Life Remaining: ${person.health}`, { fontSize: '32px', fill: '#000' });
+    scoreText.addColor("#fff", 0);
+
+    gameOver = game.add.sprite();
 }
 
 function update() {
     swatter.rotation = game.physics.arcade.moveToPointer(swatter, 60, game.input.activePointer, 500);
 
-    // game.physics.arcade.collide(pandas, person, () => {
-
-    //     person.damage(1);
-
-    // })
-
     game.physics.arcade.collide(pandas, person, () => {
+        // person.tint = 0xff0000;
         person.damage(1);
+        score -= 1;
+        scoreText.text = `Life Remaining: ${score}`;
+        // person.tint = 0xffffff;
     })
 
+
     pandas.children.forEach(child => {
-        // console.log(pandas)
         game.physics.arcade.collide( swatter, child, () => {
             child.kill();
         })
     })
-
-
-
+    if (total) total--
+    else total = 0
 }
 
 function render() {
     // game.debug.spriteInfo(swatter, 32, 32);
+    game.debug.text(`Time Remaining: ${total}`, 700, 80)
 }
