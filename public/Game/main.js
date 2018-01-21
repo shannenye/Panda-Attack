51bf3dd2c3eade0e7145ccb58a9d204e81df0450
Shannen <shannen.ye@gmail.com>
2018-01-21 17:04:22 -0500

    added red tint to sprite collision

--- public/Game/main.js
+++ public/Game/main.js
@@ -1,9 +1,11 @@
 var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-var backgroundMusic
+var backgroundMusic;
+var pandaPersonCollision;
 var swatter;
 var person;
 var pandas;
-var gameOver;
+var pandas2;
+var gameOver = {end: false};
 var score = 100;
 var scoreText;
 var timer;
@@ -13,8 +15,8 @@ function preload() {
     game.load.image('background', '../images/bamboo.jpg');
     game.load.image('zookeeper', '../images/zookeeper.png');
     game.load.image('heart', '../images/heart.png');
-    // game.load.audio('meow', '../audio/meow1.mp3');
     game.load.audio('gameSound', '../audio/gameSound.mp3');
+    game.load.audio('meow', '../audio/meow1.mp3');
     game.load.image('swatter', '../images/pandaSwatter.png');
     game.load.spritesheet('pandas', '../images/pandas.png');
     game.load.image('gameOver', '../images/Game_Over.png');
@@ -37,7 +39,6 @@ function create() {
     person.height = 100;
     person.health = 100;
 
-
     game.physics.enable(person)
     person.body.immovable = true;
 
@@ -50,12 +51,20 @@ function create() {
     timer.start();
 
     pandas = game.add.emitter(0, 0, 250);
-    pandas.makeParticles('pandas', 10, 200, true, true);
+    pandas.makeParticles('pandas', 1000, 100, true, true);
     pandas.maxParticleSpeed.setTo(200, -400);
     pandas.bounce.setTo(1, 1);
     pandas.angularDrag = 30;
     pandas.start(false, 8000, 400);
     pandas.lifespan = 0;
+
+    pandas2 = game.add.emitter(1000, 600, 250);
+    pandas2.makeParticles('pandas', 1000, 100, true, true);
+    pandas2.maxParticleSpeed.setTo(200, -400);
+    pandas2.bounce.setTo(1, 1);
+    pandas2.angularDrag = 30;
+    pandas2.start(false, 8000, 400);
+    pandas2.lifespan = 0;
 
     game.physics.enable(swatter);
 
@@ -63,32 +72,68 @@ function create() {
 
     scoreText = game.add.text(700, 10, `Life Remaining: ${person.health}`, { fontSize: '32px', fill: '#000' });
     scoreText.addColor("#fff", 0);
-
-
 }
 
 function update() {
-    swatter.rotation = game.physics.arcade.moveToPointer(swatter, 60, game.input.activePointer, 500);
+    swatter.rotation = game.physics.arcade.moveToPointer(swatter, 1000, game.input.activePointer, 100);
 
     game.physics.arcade.collide(pandas, person, () => {
-        // person.tint = 0xff0000;
-        person.damage(1);
-        score -= 1;
-        scoreText.text = `Life Remaining: ${score}`;
-        // person.tint = 0xffffff;
-    })
+        setTimeout((() => {
+            person.tint = 0xffffff;
+        }), 20);
+        person.tint = 0xff0000
+            person.damage(1);
+            score -= 1;
+        })
 
+    game.physics.arcade.collide(pandas2, person, () => {
+            setTimeout((() => {
+                person.tint = 0xffffff;
+                // person.tint = 0xff0000
+            }), 10);
+            // person.tint = 0xffffff;
+            person.tint = 0xff0000
+                person.damage(1);
+                score -= 1;
+        })
+
+    scoreText.text = `Life Remaining: ${score}`;
 
     pandas.children.forEach(child => {
         game.physics.arcade.collide( swatter, child, () => {
             child.kill();
         })
     })
-    if (total) total--
-    else total = 0
+
+    pandas2.children.forEach(child => {
+        game.physics.arcade.collide( swatter, child, () => {
+            child.kill();
+        })
+    })
+
+    if (total && person.health) {
+        total--
+    } else if (total && person.health === 0) {
+        // game over (stop time)
+        game.stop(null, true)
+    }
+    // else if (!total && person.health !== 0) {
+
+    // } // you win! Stop time (stop game)
 }
 
 function render() {
-    // game.debug.spriteInfo(swatter, 32, 32);
-    game.debug.text(`Time Remaining: ${total}`, 700, 80)
+    game.debug.text(`Time Remaining: ${total}`, 700, 80);
+    // game.debug.text(`Life Remaining: ${}`);
+
 }
+
+
+
+// want to add GAME OVER if life remaining === 0 AND time remaining !== 0
+// want to add YOU WIN if life remaining !== 0 AND time remaining === 0
+// want to add start button
+
+// if (total && person.health === 0) gameOver and stop time
+// else if (!total && person.health !== 0) you win and stop life
+// else if (total && person.health) total--
